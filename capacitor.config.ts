6844077d1c71ecc9ddc435fba71ccaec9023380f
Peter Szemán <peterszeman@Peters-MacBook-Air.local>
2021-07-26 09:19:26 +0200

    original

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.track.app',
+  appName: 'Trial band',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
